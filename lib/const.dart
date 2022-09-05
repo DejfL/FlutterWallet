0a6ca8b8c9f0d6f403f701ba2046326f3f9b3279
DejfL <dawidszymczak92@gmail.com>
2022-09-05 13:43:37 +0200

    zrobione

---
+++ lib/const.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+const primaryColor = Color.fromRGBO(0, 23, 31, 1);
+const secondaryColor = Color.fromRGBO(0, 52, 89, 1);
+const List<dynamic> transactions = [
+  {
+    'name': 'Airbnb',
+    'day': 'Monday,25 January',
+    'amount': 124,
+  },
+  {
+    'name': 'Amazon',
+    'day': 'Monday,24 January',
+    'amount': -300.19,
+  },
+  {
+    'name': 'Paypal',
+    'day': 'Monday,21 January',
+    'amount': -50,
+  },
+  {
+    'name': 'Klaudia',
+    'day': 'Monday,25 January',
+    'amount': 500.13,
+  },
+];
